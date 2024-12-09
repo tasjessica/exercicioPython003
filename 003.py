43b22aa8160c2d335472bcfbc8d4db86584e5fb2
Jessica Andrade <jessicatandrade@live.com>
2024-12-09 12:30:48 -0300

    first commit

---
+++ 003.py
@@ -0,0 +1,6 @@
+#crie um programa que leia dois números e mostre a soma entre eles
+
+n1 = int(input('Digite um numero: '))
+n2 = int(input('Digite outro número: '))
+res = n1 + n2
+print('Os números que você digitou foram {} e {} respectivamente, e a soma entre eles é {}'.format(n1, n2, res))
